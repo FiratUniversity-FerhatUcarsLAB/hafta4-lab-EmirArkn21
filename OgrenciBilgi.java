d97798d1472bfa14dfed3742e2f0f6f7454be262
EmirArkn21 <250541005@firat.edu.tr>
2025-11-06 17:55:21 +0300

    Update OgrenciBilgi.java

--- OgrenciBilgi.java
+++ OgrenciBilgi.java
@@ -1,7 +1,7 @@
 /*
- * Ad Soyad: [ADINIZI BURAYA YAZIN]
- * Ogrenci No: [OGRENCI NUMARANIZI BURAYA YAZIN]
- * Tarih: [TARIHI BURAYA YAZIN]
+ * Ad Soyad: Emirhan Arıkan
+ * Ogrenci No: 250541005
+ * Tarih: 06.11.25
  * Aciklama: Gorev 1 - Ogrenci Bilgi Sistemi
  * 
  * Bu program kullanicidan ogrenci bilgilerini alir ve
@@ -13,48 +13,32 @@ import java.util.Scanner;
 
 public class OgrenciBilgi {
     public static void main(String[] args) {
-        // Scanner objesi olusturun
-        
-        
-        // Degisken tanimlamalari
-        // String ad, soyad;
-        // int ogrenciNo, yas;
-        // double gpa;
-        
-        
-        // Kullanicidan bilgileri alin
-        System.out.println("=== OGRENCI BILGI SISTEMI ===");
-        System.out.println();
-        
-        // Ad
-        
-        
-        // Soyad
-        
-        
-        // Ogrenci No
-        
-        
-        // Yas
-        
-        
-        // GPA
-        
-        
-        // Bilgileri ekrana yazdirin
-        System.out.println("\n=== OGRENCI BILGI SISTEMI ===");
-        
-        // COZUMUNUZU BURAYA YAZIN
-        
-        
-        
-        
-        
-        
+        final double BASARI_SINIRI = 2.50;
+        Scanner input = new Scanner(System.in);
 
-        
-        
-        // Scanner'i kapatin (önemli pratik)
-        
+        System.out.printf("=== OGRENCI BILGI SISTEMI ===%n");
+
+        System.out.printf("Adinizi girin: ");
+        String ad = input.next();
+
+        System.out.printf("Soyadinizi girin: ");
+        String soyad = input.next();
+
+        System.out.printf("Ogrenci numaraniz: ");
+        int ogrenciNo = input.nextInt();
+
+        System.out.printf("Yasiniz: ");
+        int yas = input.nextInt();
+
+        System.out.printf("GPA (0.00-4.00): ");
+        double gpa = input.nextDouble();
+
+        System.out.printf("%n=== OGRENCI BILGI SISTEMI ===%n");
+        System.out.printf("Ad Soyad: %s %s%n", ad, soyad);
+        System.out.printf("Ogrenci No: %d%n", ogrenciNo);
+        System.out.printf("Yas: %d%n", yas);
+        System.out.printf("GPA: %.2f%n", gpa);
+
+        input.close();
     }
 }
